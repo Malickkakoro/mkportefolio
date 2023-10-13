d4d3d9bf29509c189187d4f9ca42536a53bdcdfa
Malickkakoro <kakoro.malick5@gmail.com>
2023-10-13 23:58:21 +0000

    Retouche

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 /*!
-* Start Bootstrap - Personal v1.0.1 (https://startbootstrap.com/template-overviews/personal)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-personal/blob/master/LICENSE)
+* Start Bootstrap - Personal v1.0.1 (https://malickkakoro.github.io/kakoro-portofolio/)
+* Copyright 2023 Start Bootstrap
+* Licensed under MIT (https://malickkakoro.github.io/kakoro-portofolio/)
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
